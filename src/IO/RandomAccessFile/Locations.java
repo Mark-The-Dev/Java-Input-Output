d8adfa1794aa39efdeef02bb3d7263e534430a4c
Mark <SwiftTactics@gmail.com>
2021-04-14 13:57:20 -0400

    recreated locations_rand.dat, refacotred main, RandomAccessMain works as intended

--- src/IO/RandomAccessFile/Locations.java
+++ src/IO/RandomAccessFile/Locations.java
@@ -81,7 +81,7 @@ public class Locations implements Map<Integer, Location> {
 
         }
 
-        }
+    }
 
         // Using Random Access File to load!
         // 1. The first four bytes will contain the number of locations (bytes 0-3)
@@ -114,7 +114,8 @@ public class Locations implements Map<Integer, Location> {
 
     }
 
-    public Location getLocation(int locationId) throws IOException{
+    public Location getLocation(int locationId) throws IOException {
+
         IndexRecord record = index.get(locationId);
         ra.seek(record.getStartByte());
         int id = ra.readInt();
@@ -124,16 +125,16 @@ public class Locations implements Map<Integer, Location> {
 
         Location location = new Location(locationId, description, null);
 
-        if(locationId != 0){
-            for (int i =0; i<exitPart.length; i++){
+        if(locationId != 0) {
+            for(int i=0; i<exitPart.length; i++) {
                 System.out.println("exitPart = " + exitPart[i]);
                 System.out.println("exitPart[+1] = " + exitPart[i+1]);
                 String direction = exitPart[i];
                 int destination = Integer.parseInt(exitPart[++i]);
                 location.addExit(direction, destination);
-
             }
         }
+
         return location;
     }
 
